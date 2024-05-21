2e8a8a23b50cfcd80c7c08352adec6b33a263717
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-21 09:51:16 +0300

    Logger App Project

---
+++ LoggerApp/LoggerApp.API/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoggerApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly LoggerApp.Domain.Abstractions.ILogger _logger;
+
+        public UserController(Domain.Abstractions.ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            _logger.Log("All Users Sent");
+            return Ok("Users");
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            _logger.Log("User Added");
+            return Ok();
+        }
+    }
+}
